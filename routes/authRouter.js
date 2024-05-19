e917111847e6ec376c20da813878557f44d677c7
ValeriiaProkhina <applehouse1883@icloud.com>
2024-05-19 22:12:50 +0300

    HW05

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -9,20 +9,29 @@ import {
   login,
   logout,
   register,
+  updateAvatar,
   updateSubscription,
 } from "../controllers/authControllers.js";
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
 import authenticate from "../middlewares/authenticate.js";
+import upload from "../middlewares/upload.js";
 
 const authRouter = express.Router();
-authRouter.post("/register", validateBody(usersSchema), register);
-authRouter.post("/login", validateBody(usersSchema), login);
-authRouter.post("/logout", authenticate, logout);
-authRouter.get("/current", authenticate, current);
+authRouter.post("/register", validateBody(usersSchema), ctrlWrapper(register));
+authRouter.post("/login", validateBody(usersSchema), ctrlWrapper(login));
+authRouter.post("/logout", authenticate, ctrlWrapper(logout));
+authRouter.get("/current", authenticate, ctrlWrapper(current));
 authRouter.patch(
   "/subscription",
   authenticate,
   validateBody(updateSubscriptionSchema),
-  updateSubscription
+  ctrlWrapper(updateSubscription)
+);
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrlWrapper(updateAvatar)
 );
 
 export default authRouter;
